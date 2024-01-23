49da749a7d970028bbbedcb96fbae72d635f557b
nikse1in <145673388+nikse1in@users.noreply.github.com>
2024-01-23 16:00:27 +0000

    Update script.js

--- script.js
+++ script.js
@@ -9,28 +9,47 @@ const emojis = [
 ];
 
 function generateReferralLink() {
+    var twitterHandle = document.getElementById('twitterHandle').value.trim();
+    var email = document.getElementById('email').value.trim();
     var loadingDiv = document.getElementById('loading');
-    var twitterHandle = document.getElementById('twitterHandle').value;
-    if (twitterHandle.trim() === '') {
-        alert('Please enter a Twitter handle.');
+
+    // Check if the fields are filled
+    if (twitterHandle === '' || email === '') {
+        alert('Please enter both your Twitter handle and email.');
         return;
     }
+
+    // Validate email format
+    if (!validateEmail(email)) {
+        alert('Please enter a valid email address.');
+        return;
+    }
+
+    // Remove '@' from Twitter handle if present
     if (twitterHandle.startsWith('@')) {
         twitterHandle = twitterHandle.substring(1);
     }
+
+    // Display loading message
     loadingDiv.classList.remove('hidden');
     setTimeout(() => {
+        // Generate and display referral link
         var referralLink = "https://apps.apple.com/app/apple-store/id6456525694?pt=126535287&ct=" + twitterHandle + "&mt=8";
         document.getElementById('referralLink').value = referralLink;
         document.getElementById('referralLinkContainer').classList.remove('hidden');
         loadingDiv.classList.add('hidden');
-    }, 1500); // Adjust the timeout as needed for loading effect
+    }, 1500); // Adjust the timeout for loading effect as needed
+}
+
+function validateEmail(email) {
+    var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
 }
 
 function copyReferralLink() {
     var referralLink = document.getElementById('referralLink');
     referralLink.select();
     document.execCommand('copy');
-    // Random emoji feedback
-    document.getElementById('emojiFeedback').innerText = emojis[Math.floor(Math.random() * emojis.length)] + ' Thank you! Referral code copied.';
+    // Display a random emoji after copying
+    document.getElementById('emojiFeedback').innerText = emojis[Math.floor(Math.random() * emojis.length)] + ' Copied!';
 }
